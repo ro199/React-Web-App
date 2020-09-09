47ecb0c1fd4d8e2fd64d6b8dbe420d9488b1dec0
Ronald Alvarado <ronald.alvarado@epn.edu.ec>
2020-09-09 11:57:46 -0500

    Updates

---
+++ precache-manifest.51a7e1c4864bcf121d4279499631156a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c5a66d7233719af77e",
+    "url": "/ro199/React-Web-App/static/js/main.78c5a66d.chunk.js"
+  },
+  {
+    "revision": "5103cf6c9b49b2837c80",
+    "url": "/ro199/React-Web-App/static/js/runtime~main.5103cf6c.js"
+  },
+  {
+    "revision": "3bca9ab3a0d8b47333c8",
+    "url": "/ro199/React-Web-App/static/js/2.3bca9ab3.chunk.js"
+  },
+  {
+    "revision": "4b9e825a85e7ef6e14c9f91c13f63535",
+    "url": "/ro199/React-Web-App/index.html"
+  }
+];
